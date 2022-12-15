8d956286397f681cba82eb561ca1e3bc040245e1
lidankatzav <lidankatzav5@gmail.com>
2022-12-15 02:08:36 +0200

    header

--- wordle/src/components/Topbar.tsx
+++ wordle/src/components/Topbar.tsx
@@ -1,63 +1,54 @@
 import React from "react";
-import '../css/topbar.css';
 
 export function Topbar() {
     return (
-        <header>
-        <div className="px-3 py-2 text-bg-dark">
-          <div className="container">
-            <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-              <a href="/" className="d-flex align-items-center my-2 my-lg-0 me-lg-auto text-white text-decoration-none">
-                <svg className="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"><use xlinkHref="#bootstrap"/></svg>
-                <img className = "logo" src = "https://camo.githubusercontent.com/9633ba7687fe294301734b7516c64a92cbd756558850330837115ace9c610f3a/68747470733a2f2f692e696d6775722e636f6d2f795870526636302e706e67"/>
-              </a>
-              <ul className="nav col-12 col-lg-auto my-2 justify-content-center my-md-0 text-small">
-                <li>
-                  <a href="#" className="nav-link text-secondary">
-                    <svg className="bi d-block mx-auto mb-1" width="24" height="24"><use xlinkHref="#home"/></svg>
-                    Home
-                  </a>
-                </li>
-                <li>
-                  <a href="#" className="nav-link text-white">
-                    <svg className="bi d-block mx-auto mb-1" width="24" height="24"><use xlinkHref="#speedometer2"/></svg>
-                    Dashboard
-                  </a>
-                </li>
-                <li>
-                  <a href="#" className="nav-link text-white">
-                    <svg className="bi d-block mx-auto mb-1" width="24" height="24"><use xlinkHref="#table"/></svg>
-                    Orders
-                  </a>
-                </li>
-                <li>
-                  <a href="#" className="nav-link text-white">
-                    <svg className="bi d-block mx-auto mb-1" width="24" height="24"><use xlinkHref="#grid"/></svg>
-                    Products
-                  </a>
-                </li>
-                <li>
-                  <a href="#" className="nav-link text-white">
-                    <svg className="bi d-block mx-auto mb-1" width="24" height="24"><use xlinkHref="#people-circle"/></svg>
-                    Customers
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-        <div className="px-3 py-2 border-bottom mb-3">
-          <div className="container d-flex flex-wrap justify-content-center">
-            <form className="col-12 col-lg-auto mb-2 mb-lg-0 me-lg-auto" role="search">
-              <input type="search" className="form-control" placeholder="Search..." aria-label="Search"/>
-            </form>
-    
-            <div className="text-end">
-              <button type="button" className="btn btn-light text-dark me-2">Login</button>
-              <button type="button" className="btn btn-primary">Sign-up</button>
-            </div>
-          </div>
-        </div>
-      </header>
+    <nav className="navbar navbar-expand-lg navbar bg-dark">
+    <div className="container-fluid justify-content-between">
+    {/* Left element: log in */}
+    <div className="d-flex">
+        <button type="button" className="btn btn-dark btn-lg btn-floating">
+            <i className="fas fa-gem">
+                <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" className="bi bi-person-circle" viewBox="0 0 16 16">
+                <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
+                <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
+                </svg> 
+            </i>
+        </button>
+    </div>
+
+    {/* /* Center element: wordle logo */}
+    <a className="navbar-brand me-2 mb-1 d-flex align-items-center">
+        <img
+          src="https://camo.githubusercontent.com/9633ba7687fe294301734b7516c64a92cbd756558850330837115ace9c610f3a/68747470733a2f2f692e696d6775722e636f6d2f795870526636302e706e67"
+          height="60"
+          alt="MDB Logo"
+          loading="lazy"
+        />
+      </a>
+
+    {/* /* Right elements: Info & Settings */}
+    <ul className="navbar-nav flex-row">
+      <li className="nav-item me-3 me-lg-1">
+        <button type="button" className="btn btn-dark btn-lg btn-floating">
+                <i className="fas fa-gem">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" className="bi bi-info-lg" viewBox="0 0 16 16">
+                    <path d="m9.708 6.075-3.024.379-.108.502.595.108c.387.093.464.232.38.619l-.975 4.577c-.255 1.183.14 1.74 1.067 1.74.72 0 1.554-.332 1.933-.789l.116-.549c-.263.232-.65.325-.905.325-.363 0-.494-.255-.402-.704l1.323-6.208Zm.091-2.755a1.32 1.32 0 1 1-2.64 0 1.32 1.32 0 0 1 2.64 0Z"/>
+                    </svg>
+                </i>
+        </button>
+      </li>
+      <li className="nav-item me-3 me-lg-1">
+        <button type="button" className="btn btn-dark btn-lg btn-floating">
+                <i className="fas fa-gem">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" className="bi bi-gear" viewBox="0 0 16 16">
+                    <path d="M8 4.754a3.246 3.246 0 1 0 0 6.492 3.246 3.246 0 0 0 0-6.492zM5.754 8a2.246 2.246 0 1 1 4.492 0 2.246 2.246 0 0 1-4.492 0z"/>
+                    <path d="M9.796 1.343c-.527-1.79-3.065-1.79-3.592 0l-.094.319a.873.873 0 0 1-1.255.52l-.292-.16c-1.64-.892-3.433.902-2.54 2.541l.159.292a.873.873 0 0 1-.52 1.255l-.319.094c-1.79.527-1.79 3.065 0 3.592l.319.094a.873.873 0 0 1 .52 1.255l-.16.292c-.892 1.64.901 3.434 2.541 2.54l.292-.159a.873.873 0 0 1 1.255.52l.094.319c.527 1.79 3.065 1.79 3.592 0l.094-.319a.873.873 0 0 1 1.255-.52l.292.16c1.64.893 3.434-.902 2.54-2.541l-.159-.292a.873.873 0 0 1 .52-1.255l.319-.094c1.79-.527 1.79-3.065 0-3.592l-.319-.094a.873.873 0 0 1-.52-1.255l.16-.292c.893-1.64-.902-3.433-2.541-2.54l-.292.159a.873.873 0 0 1-1.255-.52l-.094-.319zm-2.633.283c.246-.835 1.428-.835 1.674 0l.094.319a1.873 1.873 0 0 0 2.693 1.115l.291-.16c.764-.415 1.6.42 1.184 1.185l-.159.292a1.873 1.873 0 0 0 1.116 2.692l.318.094c.835.246.835 1.428 0 1.674l-.319.094a1.873 1.873 0 0 0-1.115 2.693l.16.291c.415.764-.42 1.6-1.185 1.184l-.291-.159a1.873 1.873 0 0 0-2.693 1.116l-.094.318c-.246.835-1.428.835-1.674 0l-.094-.319a1.873 1.873 0 0 0-2.692-1.115l-.292.16c-.764.415-1.6-.42-1.184-1.185l.159-.291A1.873 1.873 0 0 0 1.945 8.93l-.319-.094c-.835-.246-.835-1.428 0-1.674l.319-.094A1.873 1.873 0 0 0 3.06 4.377l-.16-.292c-.415-.764.42-1.6 1.185-1.184l.292.159a1.873 1.873 0 0 0 2.692-1.115l.094-.319z"/>
+                    </svg>
+                </i>
+        </button>
+      </li>
+    </ul>
+    </div>
+    </nav>
     );
 }
